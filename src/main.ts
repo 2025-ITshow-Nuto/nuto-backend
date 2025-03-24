bdb5a2b1d6f04701c3c773c66c58a4d9218ceba7
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-03-24 21:52:10 +0900

    api swagger 문서화

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('Nuto-API')
+    .setDescription('2025 It show NUTO')
+    .setVersion('1.0')
+    .addServer('http://localhost:3000/', 'Local environment')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
