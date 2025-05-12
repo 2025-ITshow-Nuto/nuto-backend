c3fa62b70a5ff5aa379762640eeee411df495734
오지은 <w2317@e-mirim.hs.kr>
2025-05-12 13:03:05 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync(
+      '/etc/letsencrypt/live/nuto.mirim-it-show.site/privkey.pem',
+    ),
+    cert: fs.readFileSync(
+      '/etc/letsencrypt/live/nuto.mirim-it-show.site/fullchain.pem',
+    ),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'https://nuto-frontend.vercel.app',
-    ],
+    origin: ['http://localhost:3001', 'https://nuto-frontend.vercel.app'],
     credentials: true,
   });
 
@@ -22,6 +32,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(443, '0.0.0.0');
 }
 bootstrap();
